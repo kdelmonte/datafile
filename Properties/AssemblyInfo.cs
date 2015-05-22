5609e1fa14b5067d5350d630a69fcf3575ecf462
Kelvin Del Monte <kdelmonten@gmail.com>
2015-05-21 21:43:34 -0400

    Add to nuget

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataFile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Make any plain, boring data file into a awesome, queryable table...")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kelvin Del Monte")]
 [assembly: AssemblyProduct("DataFile")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
