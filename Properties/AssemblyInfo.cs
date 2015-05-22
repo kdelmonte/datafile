f5c7b3dc105cd3fe585f65db1bd0087ac8f98ab1
Kelvin Del Monte <kdelmonten@gmail.com>
2015-05-21 21:45:28 -0400

    Update assembly description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataFile")]
-[assembly: AssemblyDescription("Make any plain, boring data file into a awesome, queryable table...")]
+[assembly: AssemblyDescription("Make any plain, boring data file into an awesome, queryable table...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kelvin Del Monte")]
 [assembly: AssemblyProduct("DataFile")]
